1c7d231a4f627fee179838346d9c8f4d4c8375b9
fareeha09 <ftabassum@stuy.edu>
2019-03-19 14:52:06 -0400

    fixed box

--- draw.py
+++ draw.py
@@ -8,15 +8,14 @@ from matrix import *
   # ====================
 def add_box( points, x, y, z, width, height, depth ):
     add_edge( points, x, y, z, x + width, y, z)
-    add_edge( points, x + width, y, z, x + width, y + height, z)
-    add_edge( points, x + width, y + height, z, x, y+height, z)
-    add_edge( points, x, y+height, z, x, y, z)
-    add_edge( points, x, y+height, z, x, y+height, z-depth)
-    add_edge( points, x, y+height, z-depth, x+width, y+height, z-depth)
-    add_edge( points, x+width, y+height, z-depth, x + width, y + height, z)
-    add_edge( points, x+width, y, z, x + width, y, z-depth)
-    add_edge( points, x+width, y, z, x + width, y, z-depth)
-    add_edge( points, x + width, y, z-depth, x+width, y+height, z-depth)
+    add_edge( points, x + width, y, z, x + width, y - height, z)
+    add_edge( points, x + width, y - height, z, x, y-height, z)
+    add_edge( points, x, y-height, z, x, y, z)
+    add_edge( points, x, y-height, z, x, y-height, z-depth)
+    add_edge( points, x, y, z-depth, x+width, y, z-depth)
+    add_edge( points, x+width, y, z-depth, x + width, y, z)
+    add_edge( points, x, y, z, x, y, z-depth)
+    add_edge( points, x, y, z-depth, x, y-height, z-depth)
 
 
 '''
